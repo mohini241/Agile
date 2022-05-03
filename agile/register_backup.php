838992de24def3354b3747ae288adc45a4e073ec
mohini241 <72444046+mohini241@users.noreply.github.com>
2022-05-03 19:16:21 +0530

    Add files via upload

---
+++ agile/register_backup.php
@@ -0,0 +1,50 @@
+<?php include('server.php') 
+include 'connection.php';?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>Register</h2>
+  </div>
+	
+  <form method="post" action="register.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  	  <label>Username</label>
+  	  <input type="text" name="username" value="<?php echo $username; ?>">
+  	</div>
+  	<div class="input-group">
+  	  <label>Email</label>
+  	  <input type="email" name="email" value="<?php echo $email; ?>">
+  	</div>
+  	<div class="input-group">
+  	  <label>Password</label>
+  	  <input type="password" name="password_1">
+  	</div>
+  	<div class="input-group">
+  	  <label>Confirm password</label>
+  	  <input type="password" name="password_2">
+  	</div>
+      <div class="input-group">
+        <label for="usertype">UserType</label>
+  <select name="usertype" id="usertype">
+  <option value="">--Select User Type--</option>
+    <option value="scrummaster">Scrum Master</option>
+    <option value="teammember">Team Member</option>
+   
+  </select>
+  	</div>
+  	<div class="input-group">
+  	  <button type="submit" class="btn" name="reg_user">Register</button>
+  	</div>
+  	<p>
+  		Already a member? <a href="login.php">Sign in</a>
+  	</p>
+  </form>
+</body>
+</html>
